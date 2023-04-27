543fe84731ce60e397165cdc065d3ffd79cc200a
Gadjimurad Aliev <BeanOfficial@list.ru>
2023-04-27 13:41:36 +0300

    Сделал задачу 28

---
+++ Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int multiNumbers = MultiNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {multiNumbers}");
+
+int MultiNumbers(int num)
+{
+    int multi = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            multi = multi * i;
+        }
+        
+    }
+    return multi;
+}
